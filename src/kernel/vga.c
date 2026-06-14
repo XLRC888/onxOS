2276d2de5cf455dbf19220a3d29b8754c4981608
XLRC888 <181596240+XLRC888@users.noreply.github.com>
2026-06-14 15:36:44 +0300

    some changes

--- src/kernel/vga.c
+++ src/kernel/vga.c
@@ -110,6 +110,7 @@ void vga_hw_reset(void) {
     for (int i = 0; i < VW * VH * 2; i += 2) { vga[i] = ' '; vga[i + 1] = 0x07; }
 }
 void vga_init(void) {
+    vga_hw_reset();
     memcpy(grub_save, vb, VW * VH * 2);
     vga_clear(); uc();
 }
