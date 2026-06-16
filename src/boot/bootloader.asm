7604e5ee13ede7b22721cdc28ecb1f786a97e65e
XLRC888 <181596240+XLRC888@users.noreply.github.com>
2026-06-16 22:24:30 +0300

    some changes

--- src/boot/bootloader.asm
+++ src/boot/bootloader.asm
@@ -1,7 +1,6 @@
 ORG 0x7C00
 BITS 16
 
-; stage1: MBR loaded by BIOS at 0x7C00
     cli
     xor ax, ax
     mov ds, ax
@@ -10,7 +9,6 @@ BITS 16
     mov sp, 0x7C00
     mov [boot_drive], dl
 
-    ; load stage2 (sectors 1-5) to 0x7E00 via extended read
     mov si, dap_stage2
     mov ah, 0x42
     int 0x13
@@ -39,16 +37,12 @@ dap_stage2:
     dd 0
 
 times 446-($-$$) db 0
-; partition table (filled by setup)
 times 64 db 0
 dw 0xAA55
 
-; stage2: loaded at 0x7E00, entered with DL = boot drive
 stage2:
-    ; patched entry point (first 4 bytes of stage2)
     kern_entry: dd 0x200020
 
-    ; save boot drive
     mov [st2_drive], dl
 
     mov si, dap_kern
@@ -67,21 +61,17 @@ stage2:
     hlt
     jmp .kerr
 .kdone:
-    ; enable A20 via fast A20
     in al, 0x92
     or al, 2
     out 0x92, al
 
-    ; load GDT
     lgdt [gdtr]
 
-    ; switch to protected mode
     cli
     mov eax, cr0
     or al, 1
     mov cr0, eax
 
-    ; far jump to reload CS
     jmp 0x08:pm_entry
 
 BITS 32
@@ -92,14 +82,12 @@ pm_entry:
     mov ss, ax
     mov esp, 0x10000
 
-    ; copy kernel from 0x10000 to 0x200000 (122 sectors = 62464 bytes)
     mov esi, 0x10000
     mov edi, 0x200000
     mov ecx, 62464
     cld
     rep movsb
 
-    ; jump to kernel entry point
     mov eax, [kern_entry]
     jmp eax
 
